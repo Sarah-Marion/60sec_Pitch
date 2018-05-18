9be3f5b8df0053ec0c9a0b84aea8ba89e38ad6f8
Sarah-Marion <devsarahmarion@gmail.com>
2018-05-18 18:14:55 +0300

    created a form for subscribing to the page

--- app/main/forms.py
+++ app/main/forms.py
@@ -36,3 +36,23 @@ class SignUpForm(FlaskForm):
     password = PasswordField('Password', validators = [Required(),EqualTo('password_confirm',message = 'Passwords must match')])
     password_confirm = PasswordField('Confirm Password', validators = [Required()])
     submit  = SubmitField("signup")
+
+
+    def validate_email(self, data_field):
+        if User.query.filter_by(email = data_field.data).first():
+            raise ValidationError("Sorry!..There's an account with that email")
+
+    def validate_username(self, data_field):
+        if User.query.filter_by(username = data_field.data).first():
+            raise ValidationError("Sorry!.. That username already exists")
+
+
+class SubscribeForm(FlaskForm):
+
+    email = StringField("Email:", validators=[Required(), Email()])
+    submit = SubmitField("subscribe")
+
+
+    def validate_email(self, data_field):
+        if Subscribe.query.filter_by(email = data_field.data).first():
+            raise ValidationError("Sorry!..That Email is already Subscribed")
