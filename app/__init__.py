3b03f083ac8d315e64eb48a2f0fda97d0a419200
Sarah-Marion <devsarahmarion@gmail.com>
2018-05-18 16:14:31 +0300

    added relevant importation packages and modules within them

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,14 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from config import config_options
+from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_uploads import UploadSet, configure_uploads, IMAGES
+from flask_mail import Mail
+from flask_simplemde import SimpleMDE
 
-login_manager.init_app(app)
+
+login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'main.login'
 
