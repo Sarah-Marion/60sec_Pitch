7c6c8b366dd2804f1f5461a60b5550973e972f87
Sarah-Marion <devsarahmarion@gmail.com>
2018-05-19 11:15:09 +0300

    updated the service wrapper

--- app/auth.py
+++ app/auth.py
@@ -45,3 +45,39 @@ class GoogleSignIn(OAuthSignIn):
         else:
             return (user['name'],
                     user['email'])
+
+
+def new_decoder(payload):
+    return json.loads(payload.decode('utf-8'))
+
+class OAuthSignIn(object):
+    providers = None
+
+    def __init__(self,provider_name):
+        self.provider_name = provider_name
+        credentials = app.config['OAUTH_CREDENTIALS'][provider_name]
+        self.consumer_id = credentials['id']
+        self.consumer_secret = credentials['secret']
+
+
+    def authorize(self):
+        pass
+
+    def callback(self):
+        pass
+
+    def get_callback_url(self):
+        return url_for('main.oauth_callback', provider=self.provider_name,
+                        _external=True)
+
+
+
+    @classmethod
+    def get_provider(self, provider_name):
+        
+        if self.providers is None:
+            self.providers = {}
+            for provider_class in self.__subclasses__():
+                provider = provider_class()
+                self.providers[provider.provider_name] = provider
+        return self.providers[provider_name]
