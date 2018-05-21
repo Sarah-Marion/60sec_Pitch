a2be3b3611c4713c3fe0a14a47644adebdd9da97
Sarah-Marion <devsarahmarion@gmail.com>
2018-05-21 14:13:52 +0300

    created configurations of the parent and child class

--- config.py
+++ config.py
@@ -7,16 +7,39 @@ class Config:
     #pass
     SECRET_KEY = os.environ.get("SECRET_KEY")
     DATABASE_PASSWORD = os.environ.get("DATABASE_PASSWORD")
-    SQLALCHEMY_DATABASE_URI = f'postgresql+psycopg2://marion:{os.environ.get("DATABASE_PASSWORD")}@localhost/60secPitch'
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:postgres@localhost/pitch'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
+    ADMIN_PASSWORD = os.environ.get('ADMIN_PASSWORD')
+    ADMIN_USERNAME = os.environ.get('ADMIN_USERNAME')
+
+
+    GOOGLE_LOGIN_CLIENT_ID = os.environ.get('GOOGLE_CLIENT_ID')
+    GOOGLE_LOGIN_CLIENT_SECRET = os.environ.get('GOOGLE_CLIENT_SECRET')
+
+    OAUTH_CREDENTIALS={
+    'google': {
+    'id': GOOGLE_LOGIN_CLIENT_ID,
+    'secret': GOOGLE_LOGIN_CLIENT_SECRET
+        }
+    }
+
+    POSTS_PER_PAGE = 1
 
     # email configurations
     MAIL_SERVER ='smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME",)
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD",'')
+    MAIL_DEFAULT_SENDER = os.environ.get("MAIL_DEFAULT_SENDER")
+
+    # Flask-User settings
+    USER_APP_NAME = "60sec Pitch"
+    USER_ENABLE_EMAIL = True
+    USER_ENABLE_USERNAME = True
+    USER_EMAIL_SENDER_NAME = USER_APP_NAME
+    USER_EMAIL_SENDER_EMAIL = "noreply@divergeglobal.com"
 
     @staticmethod
     def init_app(app):
@@ -50,9 +73,11 @@ class DevConfig(Config):
 
     DEBUG = True
 
-
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://marion:enlightenedaf03@localhost/pitch'
 
 config_options = {
     'development' : DevConfig,
-    'production' : ProdConfig
+    'production' : ProdConfig,
+    'test' : TestConfig
 }
