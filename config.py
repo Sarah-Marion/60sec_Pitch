c99b59ff948cda69711402114fd44d644e6acd53
Sarah-Marion <devsarahmarion@gmail.com>
2018-05-22 11:28:02 +0300

    made necessary changes

--- config.py
+++ config.py
@@ -27,19 +27,19 @@ class Config:
     POSTS_PER_PAGE = 1
 
     # email configurations
-    MAIL_SERVER ='smtp.googlemail.com'
+    MAIL_SERVER ='smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME",)
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD",'')
-    MAIL_DEFAULT_SENDER = os.environ.get("MAIL_DEFAULT_SENDER")
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    # MAIL_DEFAULT_SENDER = os.environ.get("MAIL_DEFAULT_SENDER")
 
     # Flask-User settings
     USER_APP_NAME = "60sec Pitch"
     USER_ENABLE_EMAIL = True
     USER_ENABLE_USERNAME = True
     USER_EMAIL_SENDER_NAME = USER_APP_NAME
-    USER_EMAIL_SENDER_EMAIL = "noreply@divergeglobal.com"
+    # USER_EMAIL_SENDER_EMAIL = "noreply@divergeglobal.com"
 
     @staticmethod
     def init_app(app):
@@ -60,7 +60,7 @@ class ProdConfig(Config):
         configuration settings
     """
     # pass
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:postgres@localhost/pitch'
 
 class DevConfig(Config):
     """
